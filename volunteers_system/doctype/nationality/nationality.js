a450b1b60f73e3e88d03a2f2757102998157b3ab
mohammad <MOHJAS97_FES@HOTMAIL.COM>
2021-07-15 13:38:21 +0300

    add doc Nationality
    and add i field

---
+++ volunteers_system/doctype/nationality/nationality.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, mohammad jasim hijji and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Nationality', {
+	// refresh: function(frm) {
+
+	// }
+});
