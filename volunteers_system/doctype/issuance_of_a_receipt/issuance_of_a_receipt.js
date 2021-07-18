3552fc7def09dc35d2e90635f22fb12397a800f1
mohammad <MOHJAS97_FES@HOTMAIL.COM>
2021-07-18 19:59:21 +0300

    add doc and new Report
    for Issuance Of a Voucher

---
+++ volunteers_system/doctype/issuance_of_a_receipt/issuance_of_a_receipt.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, mohammad jasim hijji and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Issuance of a Receipt', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ volunteers_system/doctype/issuance_of_a_receipt/test_issuance_of_a_receipt.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, mohammad jasim hijji and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestIssuanceofaReceipt(unittest.TestCase):
+	pass
